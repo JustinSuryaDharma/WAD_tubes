7a48d21055cd926de57873a5dc7dc0b541715385
DhewaRadya <radyadhewa@gmail.com>
2024-01-14 21:27:12 +0700

    add new file to fitur-1

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -10,6 +10,7 @@
 <html lang="en">
 
 <head>
+	<!-- Tambah meta CSS -->
 	<meta charset="utf-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 	<meta name="author" content="Untree.co">
